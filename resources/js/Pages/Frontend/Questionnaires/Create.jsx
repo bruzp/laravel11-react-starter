a364f2fe82bffe3ebfbaafbdb00ee8c715285fea
Bruce <bruce@ipp.ph>
2024-04-23 17:43:49 +0800

    Examination header modification.

--- resources/js/Pages/Frontend/Questionnaires/Create.jsx
+++ resources/js/Pages/Frontend/Questionnaires/Create.jsx
@@ -22,7 +22,7 @@ export default function TakeExam({ auth, questionnaire, status }) {
 
   return (
     <FrontendLayout user={auth.user}>
-      <Head title="Take Exam" />
+      <Head title={`Exam: ${questionnaire.title}`} />
 
       <div className="grid gap-0 lg:grid-cols-1 lg:gap-8 mb-3 select-none">
         <div className="flex flex-col items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
