d5c58af040e36e8bd12bf2b42dc0a3517c375a64
Bruce <bruce@ipp.ph>
2024-04-24 11:43:49 +0800

    Fixed error on user dashboard.

--- resources/js/Pages/User/Dashboard.jsx
+++ resources/js/Pages/User/Dashboard.jsx
@@ -17,12 +17,22 @@ export default function Dashboard({ auth, rank }) {
         <div className="max-w-full mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              <p>
-                You are currently ranked as <strong>{rank.rank_text}</strong>{" "}
-                overall—a fantastic achievement! Your highest score so far is an
-                impressive <strong>{rank.max_result}%</strong>. Keep up the
-                great work!
-              </p>
+              {rank ? (
+                <p>
+                  You are currently ranked as <strong>{rank.rank_text}</strong>{" "}
+                  overall—a fantastic achievement! Your highest score so far is
+                  an impressive <strong>{rank.max_result}%</strong>. Keep up the
+                  great work!
+                </p>
+              ) : (
+                <p>
+                  It looks like you haven't taken any exams yet. This is a great
+                  opportunity to start your journey and see what you can
+                  achieve! When you're ready, take your first exam, and begin
+                  unlocking your potential. We're excited to see how far you can
+                  go!
+                </p>
+              )}
             </div>
           </div>
         </div>
